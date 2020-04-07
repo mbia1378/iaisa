9f0fa18e129b1ac36729ce10990c9b5d2d3a8354
Cyrille MBIA <mbia1378@gmail.com>
2020-04-07 20:39:48 +0100

    push

---
+++ app/src/main/java/co/cyrille/mbia/MainApplication.kt
@@ -0,0 +1,45 @@
+package co.cyrille.mbia
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+import co.kyald.coronavirustracking.injection.*
+import co.kyald.coronavirustracking.utils.Constants
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+
+        initTheme()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(listOf(
+                appModule,
+                networkModule,
+                repositoryModule,
+                viewModelModule,
+                databaseModule,
+                sharedPreferencesModule
+            ))
+
+        }
+
+    }
+
+    private fun initTheme() {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        Timber.e("CURRENT THEME ${preferences.getString(Constants.PREF_THEME, "2")}")
+
+        AppCompatDelegate.setDefaultNightMode(preferences.getString(Constants.PREF_THEME, "2")?.toInt()!!)
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+}
